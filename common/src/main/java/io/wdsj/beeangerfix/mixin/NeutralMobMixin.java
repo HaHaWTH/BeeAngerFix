85d552c0030d5e8711e792d3374070ada0333b50
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2026-01-11 15:14:42 +0800

    better fix

--- common/src/main/java/io/wdsj/beeangerfix/mixin/NeutralMobMixin.java
+++ common/src/main/java/io/wdsj/beeangerfix/mixin/NeutralMobMixin.java
@@ -3,6 +3,8 @@ package io.wdsj.beeangerfix.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
 import net.minecraft.world.entity.EntityReference;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.NeutralMob;
@@ -22,9 +24,9 @@ public interface NeutralMobMixin {
                     target = "Lnet/minecraft/world/entity/NeutralMob;setPersistentAngerTarget(Lnet/minecraft/world/entity/EntityReference;)V"
             )
     )
-    private void beeAngerFix$moveLogic(NeutralMob instance, @Nullable EntityReference<LivingEntity> livingEntityEntityReference, Operation<Void> original) {
+    private void beeAngerFix$moveLogic(NeutralMob instance, @Nullable EntityReference<LivingEntity> livingEntityEntityReference, Operation<Void> original, @Share("isNewTarget") LocalBooleanRef isNewTarget) {
+        isNewTarget.set(true);
         original.call(instance, livingEntityEntityReference);
-        this.startPersistentAngerTimer();
     }
 
     @WrapOperation(
@@ -34,7 +36,7 @@ public interface NeutralMobMixin {
                     target = "Lnet/minecraft/world/entity/NeutralMob;startPersistentAngerTimer()V"
             )
     )
-    private void beeAngerFix$noStartPersistentAngerTimer(NeutralMob instance, Operation<Void> original, @Local(argsOnly = true) boolean updateAnger) {
-        if (updateAnger) original.call(instance);
+    private void beeAngerFix$noStartPersistentAngerTimer(NeutralMob instance, Operation<Void> original, @Local(argsOnly = true) boolean updateAnger, @Share("isNewTarget") LocalBooleanRef isNewTarget) {
+        if (updateAnger || isNewTarget.get()) original.call(instance);
     }
 }
